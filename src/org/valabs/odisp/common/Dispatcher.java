fa756959811eb381b02d8c6600388e8b653261ad
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-10-03 21:22:11 +0000

    A bit of redesign and lots of documentation

--- src/org/valabs/odisp/common/Dispatcher.java
+++ src/org/valabs/odisp/common/Dispatcher.java
@@ -1,16 +1,12 @@
-/*
- * $Id: Dispatcher.java,v 1.1 2003/10/02 23:16:30 valeks Exp $
- *	Dispatcher.java
- *	Common Dispatcher dispatcher interface. Defined as base interface
- *	for all classes that may be dispatcher that implements message
- * 	passing routines
- *      (C) 2003, JTT Novel-IL
- */
- 
 package com.novel.odisp.common;
 
+/** Стандартный интерфейс диспетчера, который должен реализовывать
+ * класс, который выступает в качестве ODISP диспетчера.
+ * @author Валентин А. Алексеев
+ * @author (C) 2003, НПП "Новел-ИЛ"
+ * @version $Id: Dispatcher.java,v 1.2 2003/10/03 21:22:11 valeks Exp $
+ */
 public interface Dispatcher {
 	public void sendMessage(Message message);
-	public Resource getResourceObject();
 	public Message getNewMessage(String action, String destination, String origin, int inReplyTo);
 }
