675be364b4b12d61ddcd86b1891d4f9f9813070c
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-10-12 20:07:33 +0000

    Refactored to support SimpleConfig as resource object.

--- src/org/valabs/stdobj/filelog/FileLog.java
+++ src/org/valabs/stdobj/filelog/FileLog.java
@@ -1,5 +1,7 @@
 package com.novel.stdobj.filelog;
+
 import com.novel.odisp.common.*;
+import com.novel.stdobj.simpleconfig.SimpleConfig;
 import java.io.*;
 import java.util.*;
 import java.util.regex.Pattern;
@@ -7,65 +9,75 @@ import java.util.regex.Pattern;
 /** Объект реализующий простейшую журнализацию событий согласно файлу шаблонов
 * @author Валентин А. Алексеев
 * @author (С) 2003, НПП "Новел-ИЛ"
-* @version $Id: FileLog.java,v 1.3 2003/10/07 11:13:36 valeks Exp $
+* @version $Id: FileLog.java,v 1.4 2003/10/12 20:07:33 valeks Exp $
 */
 public class FileLog extends PollingODObject {
-	private int valuesToConfigure = 2;
-	private String logFile, patternFile;
-	private int logFileMsgId, patternFileMsgId;
-	PrintWriter out;
-	String[] patterns;
-	public void handleMessage(Message msg){
-	    if(valuesToConfigure == 0){ // finaly we got all params we need to start -- fire up
-		try {
-		    File hLogFile = new File(logFile);
-		    if(!hLogFile.exists())
-			hLogFile.createNewFile();
-		    out = new PrintWriter(new FileWriter(hLogFile, true));
-		    BufferedReader pfile = new BufferedReader(new FileReader(patternFile));
-		    String s;
-		    List readedPatterns = new ArrayList();
-		    while((s = pfile.readLine()) != null)
-			readedPatterns.add(s);
-		    patterns = new String[readedPatterns.size()];
-		    patterns = (String[])readedPatterns.toArray();
-		    pfile.close();
-		    valuesToConfigure--;
-		} catch (FileNotFoundException e) {System.err.println("[w] unable to open logfile "+logFile);}
-		  catch (IOException e) {System.err.println("[w] unable to read either log file or pattern file");}
-	    }
-	    if(msg.getAction().equals("od_cleanup"))
-		cleanUp(((Integer)msg.getField(0)).intValue());
-	    else if(msg.getAction().equals("od_object_loaded") && msg.getDestination().equals(toString())){ // we are starting up -- request config values
-		Message m = dispatcher.getNewMessage("get_value","config.*", toString(), 0);
-		m.addField(new String("log_logfile"));
-		logFileMsgId = m.getId();
-		dispatcher.sendMessage(m);
-		m = dispatcher.getNewMessage("get_value","config.*", toString(), 1);
-		m.addField(new String("log_patternfile"));
-		patternFileMsgId = m.getId();
-		dispatcher.sendMessage(m);
-	    } else if(msg.getAction().equals("config_value") && msg.getDestination().equals(toString())){ // we got config value reply
-		if(msg.getFieldsCount() == 1){
-			if(msg.getReplyTo() == logFileMsgId) 
-			    logFile = (String)msg.getField(0);
-			if(msg.getReplyTo() == patternFileMsgId) 			    
-			    patternFile = (String)msg.getField(0);
-		    valuesToConfigure--;
-		}
-	    } else {
-		for(int i = 0; i< patterns.length; i++){
-		    if(Pattern.matches(patterns[i], msg.getAction()))
-			out.write("---\n"+new Date().toString() + " -- "+msg);
+    PrintWriter out;
+    List patterns;
+    public void handleMessage(Message msg){
+        log("handleMessage",""+msg);	
+        if(msg.getAction().equals("od_cleanup") && msg.getDestination().equals(getObjectName())){
+	    cleanUp(((Integer)msg.getField(0)).intValue());
+	    return;
+	}
+	if(msg.getAction().equals("od_object_loaded") && msg.getDestination().equals(getObjectName())){
+	    setMatch(".*");
+	    Message m = dispatcher.getNewMessage("od_acquire","stddispatcher", getObjectName(), 0);
+	    m.addField(new String("com.novel.stdobj.simpleconfig.SimpleConfig"));
+	    m.addField(new Boolean(true));
+	    dispatcher.sendMessage(m);
+	    return;
+	}
+	if(msg.getAction().equals("resource_acquired") && msg.getDestination().equals(getObjectName())){ // we acquired SimpleConfig resource
+	    if(msg.getFieldsCount() == 2){
+		String cl_n = (String)msg.getField(0);
+		if(cl_n.startsWith("com.novel.stdobj.simpleconfig.SimpleConfig")){
+		    SimpleConfig scfg = (SimpleConfig)msg.getField(1);
+		    scfg.readConfig("simpleconfig.dat");
+		    try {
+		        File hLogFile = new File(scfg.getValue("log_logfile"));
+		        if(!hLogFile.exists())
+		    	    hLogFile.createNewFile();
+			out = new PrintWriter(new FileWriter(hLogFile, true));
+			BufferedReader pfile = new BufferedReader(new FileReader(scfg.getValue("log_patternfile")));
+			String s;
+			patterns = new ArrayList();
+			while((s = pfile.readLine()) != null){
+			    patterns.add(s);
+			    log("patterns","adding pattern "+s);
+			}
+			pfile.close();
+		    } catch (FileNotFoundException e) {System.err.println("[w] unable to open logfile.");
+		    } catch (IOException e) {System.err.println("[w] unable to read either log file or pattern file");}
+		    Message m = dispatcher.getNewMessage("od_release", "stddispatcher", getObjectName(), msg.getId());
+		    m.addField(msg.getField(0));
+		    m.addField(scfg);
+		    dispatcher.sendMessage(m);
 		}
 	    }
 	    return;
 	}
-	public int cleanUp(int type){
+	log("doLog","1");
+        if(patterns == null || out == null)
+	    return;
+	log("doLog","2");	    
+	boolean doMatch = false;
+	Iterator it = patterns.iterator();
+	while(it.hasNext())
+	    if(Pattern.matches((String)it.next(), msg.getAction()))
+		doMatch = true;
+	log("doLog","3");
+	if(doMatch)
+	    out.write(new Date().toString() + " -- "+msg+"\n");
+	log("doLog","4");
+    }
+    public int cleanUp(int type){
+	if(out != null)
 	    out.close();
-	    return 0;
-	}
-	public FileLog(Integer id){
-	    super("log"+id);
-	}
+	doExit = true;
+	return 0;
+    }
+    public FileLog(Integer id){
+	super("log"+id);
+    }
 }
