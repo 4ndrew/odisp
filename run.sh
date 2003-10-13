e800ed0ce35b61daa7c5866d6ff22a9c0b35753a
Andrew Porokhin <andrew.porokhin@gmail.com>
2003-10-13 19:20:05 +0000

    Some fix. Added variable CLASSPATH to the environment.

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
-# $Id: run.sh,v 1.1 2003/10/07 11:01:24 valeks Exp $
-JAVA_HOME=${JAVA_HOME:-/usr/local/jdk1.4.1}
-$JAVA_HOME/bin/java com.novel.odisp.StandartDispatcher classes.cfg
+# $Id: run.sh,v 1.2 2003/10/13 19:20:05 dron Exp $
+JAVA_HOME=${JAVA_HOME:-/usr/local/jdk1.4.1}; export JAVA_HOME
+CLASSPATH=$PWD/../build/; export CLASSPATH
+CLASSES_CONF=$PWD/classes.cfg
+
+$JAVA_HOME/bin/java com.novel.odisp.StandartDispatcher $CLASSES_CONF
