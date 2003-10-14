3bf487ac7493f30407acdb75f4a1d6931e74576c
Andrew Porokhin <andrew.porokhin@gmail.com>
2003-10-14 19:21:15 +0000

    Added comments..

--- run.sh
+++ run.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
-# $Id: run.sh,v 1.4 2003/10/14 19:14:27 dron Exp $
+# $Id: run.sh,v 1.5 2003/10/14 19:21:15 dron Exp $
 JAVA_HOME=${JAVA_HOME:-/usr/local/jdk1.4.1}; export JAVA_HOME
 CLASSPATH=$CLASSPATH:$PWD/../build/; export CLASSPATH
 CLASSES_CONF=$PWD/classes.cfg
 # for debugging: 
+# ./run.sh -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n
+# jdb -attach 127.0.0.1:1039 (or other port)
 JAVAFLAGS=$*
 
 $JAVA_HOME/bin/java $JAVAFLAGS com.novel.odisp.StandartDispatcher $CLASSES_CONF
