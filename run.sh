8257d33dbb502561b282321e4742ee0ed51282f7
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-10-14 13:52:18 +0000

    little fixup

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-# $Id: run.sh,v 1.2 2003/10/13 19:20:05 dron Exp $
+# $Id: run.sh,v 1.3 2003/10/14 13:52:18 valeks Exp $
 JAVA_HOME=${JAVA_HOME:-/usr/local/jdk1.4.1}; export JAVA_HOME
-CLASSPATH=$PWD/../build/; export CLASSPATH
+CLASSPATH=$CLASSPATH:$PWD/../build/; export CLASSPATH
 CLASSES_CONF=$PWD/classes.cfg
 
 $JAVA_HOME/bin/java com.novel.odisp.StandartDispatcher $CLASSES_CONF
