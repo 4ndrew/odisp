104af0beddc65f19d6fb3885b85f16e0c80bb392
Andrew Porokhin <andrew.porokhin@gmail.com>
2003-10-14 19:14:27 +0000

    Added support for parameters (for enable debugging).

--- run.sh
+++ run.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
-# $Id: run.sh,v 1.3 2003/10/14 13:52:18 valeks Exp $
+# $Id: run.sh,v 1.4 2003/10/14 19:14:27 dron Exp $
 JAVA_HOME=${JAVA_HOME:-/usr/local/jdk1.4.1}; export JAVA_HOME
 CLASSPATH=$CLASSPATH:$PWD/../build/; export CLASSPATH
 CLASSES_CONF=$PWD/classes.cfg
+# for debugging: 
+JAVAFLAGS=$*
 
-$JAVA_HOME/bin/java com.novel.odisp.StandartDispatcher $CLASSES_CONF
+$JAVA_HOME/bin/java $JAVAFLAGS com.novel.odisp.StandartDispatcher $CLASSES_CONF
