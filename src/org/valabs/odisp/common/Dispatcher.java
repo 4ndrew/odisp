9d21a876755e58e02260b638d4b4f4c8656aa894
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-10-14 09:38:02 +0000

    Multiply message sender interface

--- src/org/valabs/odisp/common/Dispatcher.java
+++ src/org/valabs/odisp/common/Dispatcher.java
@@ -4,9 +4,10 @@ package com.novel.odisp.common;
  * класс, который выступает в качестве ODISP диспетчера.
  * @author Валентин А. Алексеев
  * @author (C) 2003, НПП "Новел-ИЛ"
- * @version $Id: Dispatcher.java,v 1.2 2003/10/03 21:22:11 valeks Exp $
+ * @version $Id: Dispatcher.java,v 1.3 2003/10/14 09:38:02 valeks Exp $
  */
 public interface Dispatcher {
 	public void sendMessage(Message message);
+	public void sendMessages(Message[] messageList);
 	public Message getNewMessage(String action, String destination, String origin, int inReplyTo);
 }
