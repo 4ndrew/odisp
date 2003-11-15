74c2bddbaac121e410b787baeda627a24c0a02a2
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-11-15 18:48:57 +0000

    Conform to Style Guide

--- src/org/valabs/odisp/StandartMessage.java
+++ src/org/valabs/odisp/StandartMessage.java
@@ -1,62 +1,94 @@
 package com.novel.odisp;
 
-import com.novel.odisp.common.*;
-import java.util.*;
+import com.novel.odisp.common.Message;
+import java.util.List;
+import java.util.ArrayList;
 import java.io.Serializable;
 
 /** Реализация стандартного сообщения для стандартного диспетчера ODISP
  * @author Валентин А. Алексеев
  * @author (C) 2003, НПП "Новел-ИЛ"
- * @version $Id: StandartMessage.java,v 1.5 2003/11/10 13:54:34 valeks Exp $
+ * @version $Id: StandartMessage.java,v 1.6 2003/11/15 18:48:57 valeks Exp $
  */
 public class StandartMessage implements Message, Serializable {
-	/** Внутренний уникальный счетчик сообщения */
-	static int id = 0;
-	private int my_id;
-	private List fields = new ArrayList();
-	private String action, destination, origin;
-	private int inReplyTo;
-	/** Реализация конструктора сообщения
-	 * @param action действие которое несет сообщение
-	 * @param destination адресат сообщения
-	 * @param origin отправитель сообщения
-	 * @param inReplyTo идентификатор сообщения на которое производится ответ
-	 */
-	public StandartMessage(String action, String destination, String origin, int inReplyTo){
-		this.action = action;
-		this.destination = destination;
-		this.inReplyTo = inReplyTo;
-		this.origin = origin;
-		my_id = id++;
-	}
-	/** Добавление произвольного объекта в тело сообщения
-	 * @param field объект который будет добавлен сообщение
-	 */
-	public void addField(Object field){
-		fields.add(field);
-	}
-	/** Выборка сохраненного в теле сообщения объекта по индексу
-	 * @param field индекс объекта
-	 */
-	public Object getField(int field){
-		return fields.get(field);
-	}
-	/** Возвращает действие которое несет сообщение */
-	public String getAction() { return action; }
-	/** Возвращает адресата сообщения */
-	public String getDestination() { return destination; }
-	/** Возвращает автора сообщения */
-	public String getOrigin() {return origin;}
-	/** Возвращает идентификатор сообщения на которое производится ответ */
-	public int getReplyTo() {return inReplyTo;}
-	/** Возвращает количество полей в сообщении */
-	public int getFieldsCount() { return fields.size();}
-	/** Возвращает уникальный идентификатор сообщения */
-	public int getId(){ return my_id; };
-	/** Представление сообщения в виде текстовой строки 
-	 * @return строчное представление сообщения
-	*/
-	public String toString(){
-	    return "stdmessage id="+my_id+" replyto="+inReplyTo+" action=" + action + ", destination=" + destination + ", origin="+origin+", fields.size()=" + fields.size();
-	}
+  /** Внутренний уникальный счетчик сообщения */
+  private static int id = 0;
+  /** Уникальный индекс сообщения в системе */
+  private int myId;
+  /** Список полей сообщения */
+  private List fields = new ArrayList();
+  /** Действие */
+  private String action;
+  /** Точка назначения */
+  private String destination;
+  /** Отправитель */
+  private String origin;
+  /** Идентификатор сообщения на которое производится ответ */
+  private int inReplyTo;
+  /** Реализация конструктора сообщения
+   * @param action действие которое несет сообщение
+   * @param destination адресат сообщения
+   * @param origin отправитель сообщения
+   * @param inReplyTo идентификатор сообщения на которое производится ответ
+   */
+  public StandartMessage(String action, String destination, String origin, int inReplyTo) {
+    this.action = action;
+    this.destination = destination;
+    this.inReplyTo = inReplyTo;
+    this.origin = origin;
+    myId = id++;
+  }
+  /** Добавление произвольного объекта в тело сообщения
+   * @param field объект который будет добавлен сообщение
+   */
+  public void addField(Object field) {
+    fields.add(field);
+  }
+  /** Выборка сохраненного в теле сообщения объекта по индексу
+   * @param field индекс объекта
+   * @return поле сообщения
+   */
+  public Object getField(int field) {
+    return fields.get(field);
+  }
+  /** Возвращает действие которое несет сообщение 
+   * @return действие
+   */
+  public String getAction() { 
+    return action; 
+  }
+  /** Возвращает адресата сообщения 
+   * @return адресат
+   */
+  public String getDestination() { return destination; }
+  /** Возвращает автора сообщения 
+   * @return автор
+   */
+  public String getOrigin() {
+    return origin;
+  }
+  /** Возвращает идентификатор сообщения на которое производится ответ 
+   * @return идентификатор
+   */
+  public int getReplyTo() {
+    return inReplyTo;
+  }
+  /** Возвращает количество полей в сообщении 
+   * @return кол-во полей
+   */
+  public int getFieldsCount() { 
+    return fields.size();
+  }
+  /** Возвращает уникальный идентификатор сообщения 
+   * @return идентификатор
+   */
+  public int getId() { 
+    return myId; 
+  }
+  /** Представление сообщения в виде текстовой строки 
+   * @return строчное представление сообщения
+   */
+  public String toString() {
+    return "stdmessage id=" + myId + " replyto=" + inReplyTo + " action=" + action + ", destination=" + destination + ", origin=" + origin + ", fields.size()=" + fields.size();
+  }
 }
