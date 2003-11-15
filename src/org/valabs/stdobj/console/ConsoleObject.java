c48087e61dbafc03849c727a86ab4e2e4097e792
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-11-15 19:46:00 +0000

    style(9)

--- src/org/valabs/stdobj/console/ConsoleObject.java
+++ src/org/valabs/stdobj/console/ConsoleObject.java
@@ -1,83 +1,106 @@
 package com.novel.stdobj.console;
-import com.novel.odisp.common.*;
-import java.util.regex.Pattern;
-import java.io.*;
+
+import com.novel.odisp.common.PollingODObject;
+import com.novel.odisp.common.Message;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 /** Объект ODISP реализующий консольный интерфейс доступа к менеджеру
 * @author Валентин А. Алексеев
 * @author (C) 2003, НПП "Новел-ИЛ"
-* @version $Id: ConsoleObject.java,v 1.7 2003/10/22 21:22:03 valeks Exp $
+* @version $Id: ConsoleObject.java,v 1.8 2003/11/15 19:42:16 valeks Exp $
 */
 public class ConsoleObject extends PollingODObject {
-	private Thread reader;
-	public void handleMessage(Message msg){
-	    logger.finest("processing "+msg);
-            if(msg.getAction().equals("od_object_loaded")){
-	        reader = new Thread(new Runnable() {
-	        BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
-	        public void run(){
-		        try {
-		                sleep(100);
-		                System.out.print("action> ");
-		                String action, tmp;
-		                while((action = inp.readLine()) != null){
-			                System.out.print("destination> ");
-			                Message m = dispatcher.getNewMessage(action, inp.readLine(), getObjectName(), 0);
-			                System.out.print("params? ");
-			                while(!inp.readLine().equals("")){
-			                        System.out.print("int|str? ");
-			                        tmp = inp.readLine();
-			                        System.out.print("value> ");
-			                        if(tmp.startsWith("i"))
-				                        m.addField(new Integer(inp.readLine()));
-			                        else 
-				                        m.addField(new String(inp.readLine()));
-			                        System.out.print("more? ");
-			                }
-			                getDispatcher().sendMessage(m);
-			                sleep(1);
-			                System.out.print("action> ");
-		                }
-		        } catch(IOException e) {
-		                logger.info("ConsoleReader: Terminal connection lost. Quitting.");
-		        } catch(InterruptedException e){
-		                logger.info("ConsoleReader: closing console");
-		        }
-	        }    
-	        });
-	        reader.start();
-            }
-	    if(msg.getAction().equals("od_cleanup"))
-		cleanUp(((Integer)msg.getField(0)).intValue());
-	    else {
-		System.out.println("Received:");
-		System.out.println(msg.toString());
-		if(msg.getFieldsCount()>0)
-		System.out.println("Fields dump:");
-		for(int i = 0;i<msg.getFieldsCount();i++){
-		    System.out.print(i+":");
-		    System.out.println(msg.getField(i).toString());
+  /** Поток читающий ввод с консоли */
+  private Thread reader;
+  /** Обработчик входящих сообщений 
+   * @param msg сообщение
+   */
+  public void handleMessage(Message msg) {
+    logger.finest("processing " + msg);
+    if (msg.getAction().equals("od_object_loaded")) {
+      reader = new Thread(new Runnable() {
+	  BufferedReader inp = new BufferedReader(new InputStreamReader(System.in));
+	  public void run() {
+	    try {
+	      sleep(100);
+	      System.out.print("action> ");
+	      String action, tmp;
+	      while ((action = inp.readLine()) != null) {
+		System.out.print("destination> ");
+		Message m = dispatcher.getNewMessage(action, inp.readLine(), getObjectName(), 0);
+		System.out.print("params? ");
+		while (!inp.readLine().equals("")) {
+		  System.out.print("int|str? ");
+		  tmp = inp.readLine();
+		  System.out.print("value> ");
+		  if (tmp.startsWith("i")) {
+		    m.addField(new Integer(inp.readLine()));
+		  } else {
+		    m.addField(new String(inp.readLine()));
+		  }
+		  System.out.print("more? ");
 		}
+		getDispatcher().sendMessage(m);
+		sleep(1);
+		System.out.print("action> ");
+	      }
+	    } catch (IOException e) {
+	      logger.info("ConsoleReader: Terminal connection lost. Quitting.");
+	    } catch (InterruptedException e) {
+	      logger.info("ConsoleReader: closing console");
 	    }
-	    return;
-	}
-    public int cleanUp(int type){
-	if(reader != null)
-	    reader.interrupt();
-	doExit = true;
-	return 0;
+	  }    
+	});
+      reader.start();
     }
-    public ConsoleObject(Integer id){
-	super("console"+id);
+    if (msg.getAction().equals("od_cleanup")) {
+      cleanUp(((Integer) msg.getField(0)).intValue());
+    } else {
+      System.out.println("Received:");
+      System.out.println(msg.toString());
+      if (msg.getFieldsCount() > 0) {
+	System.out.println("Fields dump:");
+      }
+      for (int i = 0; i < msg.getFieldsCount(); i++) {
+	System.out.print(i + ":");
+	System.out.println(msg.getField(i).toString());
+      }
     }
-    public String[] getProviding(){
-	String res[] = {"console"};
-	return res;
+    return;
+  }
+  /** Выход 
+   * @param type признак выхода
+   * @return код возврата
+   */
+  public int cleanUp(int type) {
+    if (reader != null) {
+      reader.interrupt();
     }
-    public String[] getDepends(){
-	String res[] = {
-	    "stddispatcher",
-	    "log"
-	};
-	return res;
-    }
+    doExit = true;
+    return 0;
+  }
+  /** Конструктор объекта с заданным порядковым номером
+   * @param id номер
+   */
+  public ConsoleObject(Integer id) {
+    super("console" + id);
+  }
+  /** Вернуть список сервисов
+   * @return список сервисов
+   */
+  public String[] getProviding() {
+    String res[] = {"console"};
+    return res;
+  }
+  /** Вернуть список зависимостей 
+   * @return список зависимостей
+   */
+  public String[] getDepends() {
+    String res[] = {
+      "stddispatcher",
+      "log"
+    };
+    return res;
+  }
 }
--- src/org/valabs/stdobj/echo/EchoObject.java
+++ src/org/valabs/stdobj/echo/EchoObject.java
@@ -1,41 +1,46 @@
 package com.novel.stdobj.echo;
-import com.novel.odisp.common.*;
-import java.util.regex.Pattern;
+
+import com.novel.odisp.common.CallbackODObject;
+import com.novel.odisp.common.Message;
+import com.novel.odisp.common.MessageHandler;
 
 /** Простейший ODISP объект реализующий автоответчик на приходящие сообщения
 * @author Валентин А. Алексеев
 * @author (C) 2003, НПП "Новел-ИЛ"
-* @version $Id: EchoObject.java,v 1.7 2003/11/10 14:24:26 valeks Exp $
+* @version $Id: EchoObject.java,v 1.8 2003/11/15 19:46:00 valeks Exp $
 */
 public class EchoObject extends CallbackODObject {
-	protected void registerHandlers(){
-	    addHandler("od_cleanup", new MessageHandler(){
-		public void messageReceived(Message msg){cleanUp(((Integer)msg.getField(0)).intValue());}
-	    });
-	    addHandler("echo", new MessageHandler(){
-		public void messageReceived(Message msg){
-		    Message m = dispatcher.getNewMessage("echo_reply",msg.getOrigin(),getObjectName(),msg.getId());
-		    for(int i = 0;i<msg.getFieldsCount();i++)
+  protected void registerHandlers() {
+    addHandler("od_cleanup", new MessageHandler() {
+	public void messageReceived(Message msg) {
+	  cleanUp(((Integer) msg.getField(0)).intValue());
+	}
+      });
+    addHandler("echo", new MessageHandler() {
+	public void messageReceived(Message msg) {
+	  Message m = dispatcher.getNewMessage("echo_reply", msg.getOrigin(), getObjectName(), msg.getId());
+	  for (int i = 0; i < msg.getFieldsCount(); i++) {
 			m.addField(msg.getField(i));
-		    dispatcher.sendMessage(m);
-		}
-	    });
+	  }
+	  dispatcher.sendMessage(m);
 	}
-	public int cleanUp(int type){
-	    return 0;
-	}
-	public EchoObject(Integer id){
-	    super("echo"+id);
-	}
-	public String[] getProviding(){
-	    String res[] = {"echo"};
-	    return res;
-	}
-	public String[] getDepends(){
-	    String res[] = {
-		"stddispatcher",
-		"log"
-	    };
-	    return res;
-	}
+      });
+  }
+  public int cleanUp(int type) {
+    return 0;
+  }
+  public EchoObject(Integer id) {
+    super("echo" + id);
+  }
+  public String[] getProviding() {
+    String res[] = {"echo"};
+    return res;
+  }
+  public String[] getDepends() {
+    String res[] = {
+      "stddispatcher",
+      "log"
+    };
+    return res;
+  }
 }
