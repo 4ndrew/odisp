9240bcef3d0358e066e6d0e1237bc5bf99b5842c
Valentin Alexeev <valentin.alekseev@gmail.com>
2003-12-03 19:23:27 +0000

    Moving FileLog to Typed Messages

--- src/org/valabs/stdobj/filelog/FileLog.java
+++ src/org/valabs/stdobj/filelog/FileLog.java
@@ -3,6 +3,7 @@ package com.novel.stdobj.filelog;
 import com.novel.odisp.common.PollingODObject;
 import com.novel.odisp.common.Message;
 import com.novel.stdobj.simpleconfig.SimpleConfig;
+import com.novel.stdmsg.*;
 import java.io.PrintWriter;
 import java.io.IOException;
 import java.io.FileNotFoundException;
@@ -19,7 +20,7 @@ import java.util.regex.Pattern;
 /** Объект реализующий простейшую журнализацию событий согласно файлу шаблонов
 * @author Валентин А. Алексеев
 * @author (С) 2003, НПП "Новел-ИЛ"
-* @version $Id: FileLog.java,v 1.10 2003/11/27 02:00:01 valeks Exp $
+* @version $Id: FileLog.java,v 1.11 2003/12/03 19:23:27 valeks Exp $
 */
 public class FileLog extends PollingODObject {
   /** Поток вывода */
@@ -27,48 +28,47 @@ public class FileLog extends PollingODObject {
   /** Список шаблонов действий */
   private List patterns;
   public void handleMessage(Message msg) {
-    if (msg.getAction().equals("od_cleanup") && msg.getDestination().equals(getObjectName())) {
-      cleanUp(((Integer) msg.getField(0)).intValue());
+    if (msg instanceof ODCleanupMessage && msg.getDestination().equals(getObjectName())) {
+      cleanUp(((ODCleanupMessage)msg).getReason());
       return;
     }
-    if (msg.getAction().equals("od_object_loaded") && msg.getDestination().equals(getObjectName())) {
+    if (msg instanceof ODObjectLoadedMessage && msg.getDestination().equals(getObjectName())) {
       setMatch(".*");
-      Message m = dispatcher.getNewMessage("od_acquire", "stddispatcher", getObjectName(), 0);
-      m.addField(new String("com.novel.stdobj.simpleconfig.SimpleConfig"));
-      m.addField(new Boolean(true));
+      ODAcquireMessage m = new ODResourceAcquire(getObjectName(), 0);
+      m.setResourceName("com.novel.stdobj.simpleconfig.SimpleConfig");
+      m.setWillBlock(true);
       dispatcher.sendMessage(m);
       return;
     }
-    if (msg.getAction().equals("resource_acquired") && msg.getDestination().equals(getObjectName())) { // we acquired SimpleConfig resource
-      if (msg.getFieldsCount() == 2) {
-	String className = (String) msg.getField(0);
-	if (className.startsWith("com.novel.stdobj.simpleconfig.SimpleConfig")) {
-	  SimpleConfig scfg = (SimpleConfig) msg.getField(1);
-	  scfg.readConfig("simpleconfig.dat");
-	  try {
-	    File hLogFile = new File(scfg.getValue("log_logfile"));
-	    if (!hLogFile.exists()) {
-	      hLogFile.createNewFile();
-	    }
-	    out = new PrintWriter(new FileWriter(hLogFile, true));
-	    BufferedReader pfile = new BufferedReader(new FileReader(scfg.getValue("log_patternfile")));
-	    String s;
-	    patterns = new ArrayList();
-	    while ((s = pfile.readLine()) != null) {
-	      patterns.add(s);
-	    }
-	    pfile.close();
-	  } catch (FileNotFoundException e) { logger.warning("unable to open logfile.");
-	  } catch (IOException e) { logger.warning("unable to read either log file or pattern file");}
-	  Message[] m = {
-	    dispatcher.getNewMessage("od_release", "stddispatcher", getObjectName(), msg.getId()),
-	    dispatcher.getNewMessage("od_remove_dep", "stddispatcher", getObjectName(), 0)
-	  };
-	  m[0].addField(msg.getField(0));
-	  m[0].addField(scfg);
-	  m[1].addField("com.novel.stdobj.simpleconfig.SimpleConfig");
-	  dispatcher.sendMessages(m);
-	}
+    if (msg instanceof ODResourceAcquiredMessage && msg.getDestination().equals(getObjectName())) { // we acquired SimpleConfig resource
+      String className = (String) ((ODResourceAcquiredMessage)msg).getClassName();
+      if (className.startsWith("com.novel.stdobj.simpleconfig.SimpleConfig")) {
+	SimpleConfig scfg = (SimpleConfig) ((ODResourceAcquiredMessage)msg).getResource();
+	scfg.readConfig("simpleconfig.dat");
+	try {
+	  File hLogFile = new File(scfg.getValue("log_logfile", "odisp.log"));
+	  if (!hLogFile.exists()) {
+	    hLogFile.createNewFile();
+	  }
+	  out = new PrintWriter(new FileWriter(hLogFile, true));
+	  BufferedReader pfile = new BufferedReader(new FileReader(scfg.getValue("log_patternfile", "odisp-log.ptn")));
+	  String s;
+	  patterns = new ArrayList();
+	  while ((s = pfile.readLine()) != null) {
+	    patterns.add(s);
+	  }
+	  pfile.close();
+	} catch (FileNotFoundException e) { logger.warning("unable to open logfile.");
+	} catch (IOException e) { logger.warning("unable to read either log file or pattern file");}
+	Message[] m = {
+	  new ODReleaseMessage(getObjectName(), msg.getId()),
+	  new ODRemoveDepMessage(),
+	  /*	  dispatcher.getNewMessage("od_release", "stddispatcher", getObjectName(), msg.getId()),
+		  dispatcher.getNewMessage("od_remove_dep", "stddispatcher", getObjectName(), 0)*/
+	};
+	((ODReleaseMessage)m[0]).setResourceName(className).setResource(scfg);
+	((ODRemoveDepMessage)m[1]).setDepName("com.novel.stdobj.simpleconfig.SimpleConfig");
+	dispatcher.sendMessages(m);
       }
       return;
     }
