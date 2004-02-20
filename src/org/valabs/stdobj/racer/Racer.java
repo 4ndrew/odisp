334f7556fdb724e1b0e89e3327c1f0c122b222e2
Valentin Alexeev <valentin.alekseev@gmail.com>
2004-02-20 00:19:44 +0000

    Insane testing only object that tries to exploit resource acquire/release bug. Currently -- succesfully :(

---
+++ src/org/valabs/stdobj/racer/Racer.java
@@ -0,0 +1,60 @@
+package com.novel.stdobj.racer;
+
+import com.novel.odisp.common.PollingODObject;
+import com.novel.stdmsg.ODResourceAcquiredMessage;
+import com.novel.stdmsg.ODAcquireMessage;
+import com.novel.stdmsg.ODReleaseMessage;
+import com.novel.stdmsg.ODCleanupMessage;
+import com.novel.stdmsg.ODObjectLoadedMessage;
+import com.novel.odisp.common.Message;
+import com.novel.odisp.common.Resource;
+
+/** Объект, который пытается воспроизвести быстрые асинхронные запросы на захват и высвобождение ресурса.
+ * @author (C) 2004 <a href="valeks@novel-il.ru">Valentin A. Alekseev</a>
+ * @version $Id: Racer.java,v 1.1 2004/02/20 00:19:44 valeks Exp $
+ */
+public class Racer extends PollingODObject {
+  public Racer(Integer id) {
+    super("racer" + id);
+  }
+  public final void handleMessage(final Message msg) {
+    if (msg instanceof ODObjectLoadedMessage) {
+      ODAcquireMessage m = new ODAcquireMessage(getObjectName(), msg.getId());
+      m.setResourceName("com.novel.stdobj.simpleconfig.SimpleConfig");
+      dispatcher.send(m);
+    } else if (msg instanceof ODCleanupMessage) {
+      cleanUp(0);
+    } else if (msg instanceof ODResourceAcquiredMessage) {
+      try {
+	sleep((int)Math.random() * 100);
+      } catch (InterruptedException e) {}
+      ODReleaseMessage m = new ODReleaseMessage(getObjectName(), msg.getId());
+      m.setResourceName((String) msg.getField(0));
+      m.setResource((Resource) msg.getField(1));
+      dispatcher.send(m);
+      try {
+	sleep((int)Math.random() * 100);
+      } catch (InterruptedException e) {}
+      ODAcquireMessage m1 = new ODAcquireMessage(getObjectName(), msg.getId());
+      m1.setResourceName("com.novel.stdobj.simpleconfig.SimpleConfig");
+      dispatcher.send(m1);
+    }
+  }
+  public int cleanUp(int code) {
+    doExit = true;
+    return code;
+  }
+  public String[] getProviding() {
+    String[] result = {
+      "racer"
+    };
+    return result;
+  }
+  public String[] getDepends() {
+    String[] result = {
+      "stddispatcher",
+      "com.novel.stdobj.simpleconfig.SimpleConfig"
+    };
+    return result;
+  }
+}
