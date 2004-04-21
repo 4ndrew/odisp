f41390f485bf9f23d49cd7b439f3affe2c0a2de6
Andrew Porokhin <andrew.porokhin@gmail.com>
2004-04-21 14:26:24 +0000

    Added resource configuration.

--- src/org/valabs/stdobj/translator/Translator.java
+++ src/org/valabs/stdobj/translator/Translator.java
@@ -11,23 +11,29 @@ import com.novel.odisp.common.Resource;
  *
  * @author <a href="mailto:dron@novel-il.ru">Андрей А. Порохин</a>
  * @author (C) 2004 НПП "Новел-ИЛ"
- * @version $Id: Translator.java,v 1.5 2004/03/31 13:11:58 dron Exp $
+ * @version $Id: Translator.java,v 1.6 2004/04/21 14:26:24 dron Exp $
  */
 public class Translator extends Properties implements Resource {
   /** Отладочный лог */
   private static final Logger logger = Logger.getLogger("com.novel.nms.client.strings");
+  /** Имя параметра, который содержит имя файла для загрузки */
+  public final static String LanguageFile = "LanguageFile";
   /** Имя файла, в дальнейшем пранируется использование начального
    * конфигурирования объектов Odisp
    */
-  private final String LanguageFileName = "resources/language/Default.lng";
+  private final String DefaultLanguageFile = "resources/language/Default_koi8-r.lng";
+  
+  /** Конфиг ресурса */
+  private Map config = null;
   
   /** Конструктор транслятора
    */
   public Translator() {
     super();
-    // Загрузка файла
+    // TODO: Подумать тут на счёт конфигурабельности...
     try {
-      FileInputStream inputStream = new FileInputStream(LanguageFileName);
+      FileInputStream inputStream = new FileInputStream
+        (getParameter(LanguageFile, DefaultLanguageFile));
       load(inputStream);
     } catch (Exception e) {
       logger.warning("Exception: " + e);
@@ -64,10 +70,28 @@ public class Translator extends Properties implements Resource {
   public int getMaxReferenceCount() {
     return 0;
   }
+  
+  /** Получение параметров ресурса.
+   * 
+   * @param key ключ.
+   * @param defValue Значение по-умолчанию.
+   * @return Если существует значение сопоставенное ключу, то оно
+   * возвращается, иначе возвращается значение по-умолчанию.
+   */
+  private String getParameter(String key, String defValue) {
+    if (config != null) {
+      Object value = config.get(key); 
+      return (value != null) ? (String) value : defValue; 
+    }
+    logger.warning("---------- Config in resources is null");
+    return defValue; 
+  }
+  
   /** Установка конфигурации ресурса.
+   * 
    * @param cfg конфигурация
    */
   public void setConfiguration(final Map cfg) {
-  
+    config = cfg;
   }
 }
