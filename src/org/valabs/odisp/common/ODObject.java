7c3b9509cd5d1a7c0091af3e8f447b9515c18677
Valentin Alexeev <valentin.alekseev@gmail.com>
2004-05-13 09:22:38 +0000

    HEADS UP! Callback and Polling are now merged into one common StandartODObject. This involves lots of changes all over the code so beware of DOGS!

--- src/org/valabs/odisp/common/ODObject.java
+++ src/org/valabs/odisp/common/ODObject.java
@@ -1,125 +1,44 @@
 package com.novel.odisp.common;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 /** Базовый класс реализующий почтовый ящик для сообщений
 * посылаемых диспетчером ODISP.
 * @author Валентин А. Алексеев
 * @author (С) 2003, НПП "Новел-ИЛ"
-* @version $Id: ODObject.java,v 1.16 2004/05/11 09:55:39 valeks Exp $
+* @version $Id: ODObject.java,v 1.17 2004/05/13 09:22:37 valeks Exp $
 */
-public abstract class ODObject /* extends Thread */ {
-  /** Журнал. */
-  protected Logger logger;
-  /** Диспетчер работающий с этим объектом. */
-  protected Dispatcher dispatcher;
-  /** Список сообщений к обработке. */
-  protected List messages;
-  /** Таблица конфигурационных параметров. */
-  private Map configuration;
-  /** Regex маска принимаемых сообщений.
-   * По умолчанию инициализируется именем объекта. */
-  protected String match;
-  /** Внутреннее имя объекта в ядре ODISP. */
-  public String name;
-  /** Изменить маску принимаемых сообщений.
-   * @param newMatch новая маска
-   */
-  protected final void setMatch(final String newMatch) {
-    match = newMatch;
-  }
-  /** Конструктор инициализирующий почтовый ящик.
-   * @param newName имя объекта
-   */
-  public ODObject(final String newName) {
-    messages = new ArrayList();
-    name = newName;
-    match = newName;
-    logger = Logger.getLogger(newName);
-  }
-  /** Доступ к диспетчеру.
-   * @return ссылка на диспетчер
-   */
-  protected final Dispatcher getDispatcher() {
-    return dispatcher;
-  }
+public interface ODObject /* extends Thread */ {
   /** Возвращает внутреннее ODISP имя объекта.
    * @return ODISP имя объекта
    */
-  public final String getObjectName() {
-    return name;
-  }
+  public String getObjectName();
   /** Устанавливает диспетчера для текущего объекта.
    * @param d диспетчер работающий с этим объектом
    */
-  public final void setDispatcher(final Dispatcher d) {
-    this.dispatcher = d;
-  }
+  public void setDispatcher(final Dispatcher d);
   /** Интерфейс добавления сообщения в ящик.
    * @param msg сообщение для добавления
    */
-  public abstract void addMessage(final Message msg);
-
-  /** Добавление списка сообщений в ящик.
-   * @param newMessages список сообщений для добавления
-   */
-  public final void addMessages(final List newMessages) {
-    Iterator it = newMessages.iterator();
-    while (it.hasNext()) {
-		addMessage((Message) it.next());
-    }
-  }
-  /** Метод который вызывает для обработки пришедшего сообщения.
-   * Подклассы обязаны реализовать этот метод для корректной работы.
-   * @param msg сообщение для обработки
-   */
-  protected abstract void handleMessage(final Message msg);
+  public void addMessage(final Message msg);
 
   /** Метод вызываемый для очистки данных класса.
    * @param type условие выхода
    * @return код возврата
    */
-  public abstract int cleanUp(final int type);
-  /** Вывод журнальных сообщений на экран.
-   * @param place участок кода к которому относится сообщение
-   * @param msg сообщение
-   * @deprecated необходимо использовать logger
-   */
-  protected final void log(final String place, final String msg) {
-    logger.fine(getObjectName() + "." + place + ": " + msg);
-  }
+  public int cleanUp(final int type);
+
   /** Выдача списка зависимостей.
    * @return массив зависимостей
    */
-  public abstract String[] getDepends();
+  public String[] getDepends();
   /** Список сервисов предоставляемых объектом.
    * @return список сервисов
    */
-  public abstract String[] getProviding();
+  public String[] getProviding();
+
   /** Установить таблицу параметров.
    * @param cfg новая таблица
    */
-  public final void setConfiguration(final Map cfg) {
-    configuration = cfg;
-  }
-  /** Получить значение параметра конфигурации.
-   * @param name имя параметра
-   */
-  protected final String getParameter(final String name) {
-    if (configuration != null && configuration.containsKey(name)) {
-      return (String) configuration.get(name);
-    }
-    return null;
-  }
-  /** Получить значение параметра конфигурации с учетом значения по-умолчанию.
-   * @param name имя параметра
-   * @param defValue значение по умолчанию
-   */
-  protected final String getParameter(final String name, final String defValue) {
-    return getParameter(name) == null ? defValue : getParameter(name);
-  }
+  public void setConfiguration(final Map cfg);
 }
---
+++ src/org/valabs/odisp/common/StandartODObject.java
@@ -0,0 +1,147 @@
+package com.novel.odisp.common;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.logging.Logger;
+import java.util.regex.Pattern;
+
+/** Стандартный объект ODISP.
+ * @author <a href="mailto:valeks@novel-il.ru">Valentin A. Alekseev</a>
+ * @version $Id: StandartODObject.java,v 1.1 2004/05/13 09:22:38 valeks Exp $
+ */
+
+public abstract class StandartODObject implements ODObject {
+  /** Журнал. */
+  protected Logger logger;
+  /** Диспетчер работающий с этим объектом. */
+  protected Dispatcher dispatcher;
+  /** Список сообщений к обработке. */
+  private List messages;
+  /** Таблица конфигурационных параметров. */
+  private Map configuration;
+  /** Regex маска принимаемых сообщений.
+   * По умолчанию инициализируется именем объекта. */
+  private String match;
+  /** Внутреннее имя объекта в ядре ODISP. */
+  private String name;
+  /** Карта обработчиков сообщений. */
+  private Map handlers;
+  /** Список сообщений пришедших до регистрации обработчиков. */
+  private List unhandledMessages = new LinkedList();
+  /** Изменить маску принимаемых сообщений.
+   * @param newMatch новая маска
+   */
+  protected final void setMatch(final String newMatch) {
+    match = newMatch;
+  }
+
+  /** Конструктор инициализирующий почтовый ящик.
+   * @param newName имя объекта
+   */
+  public StandartODObject(final String newName) {
+    messages = new ArrayList();
+    name = newName;
+    match = newName;
+    logger = Logger.getLogger(newName);
+    logger.setLevel(java.util.logging.Level.ALL);
+    handlers = new HashMap();
+    registerHandlers();
+  }
+
+  /** Доступ к диспетчеру.
+   * @return ссылка на диспетчер
+   */
+  protected final Dispatcher getDispatcher() {
+    return dispatcher;
+  }
+
+  /** Возвращает внутреннее ODISP имя объекта.
+   * @return ODISP имя объекта
+   */
+  public final String getObjectName() {
+    return name;
+  }
+
+  /** Устанавливает диспетчера для текущего объекта.
+   * @param d диспетчер работающий с этим объектом
+   */
+  public final void setDispatcher(final Dispatcher d) {
+    this.dispatcher = d;
+  }
+
+  /** Установить таблицу параметров.
+   * @param cfg новая таблица
+   */
+  public final void setConfiguration(final Map cfg) {
+    configuration = cfg;
+  }
+  /** Получить значение параметра конфигурации.
+   * @param name имя параметра
+   */
+  protected final String getParameter(final String name) {
+    if (configuration != null && configuration.containsKey(name)) {
+      return (String) configuration.get(name);
+    }
+    return null;
+  }
+  /** Получить значение параметра конфигурации с учетом значения по-умолчанию.
+   * @param name имя параметра
+   * @param defValue значение по умолчанию
+   */
+  protected final String getParameter(final String name, final String defValue) {
+    return getParameter(name) == null ? defValue : getParameter(name);
+  }
+
+  /** Регистрация обработчика сообщения.
+   * @param message символьное имя сообщения
+   * @param handler обработчик
+   */
+  protected final void addHandler(final String message,
+				  final MessageHandler handler) {
+    if (handlers.containsKey(message)) {
+      return;
+    }
+    handlers.put(message, handler);
+  }
+
+  /** Интерфейс добавления сообщения в ящик.
+   * @param msg сообщение
+   */
+  public final void addMessage(final Message msg) {
+    if (!Pattern.matches(match, msg.getDestination())
+	&& !Pattern.matches(msg.getDestination(), getObjectName())) {
+      return;
+    }
+    handleMessage(msg);
+  }
+
+  /** Обработка сообщения.
+   * @param msg сообщение для обработки
+   */
+  protected void handleMessage(final Message msg) {
+    if (handlers.containsKey(msg.getAction())) {
+      ((MessageHandler) handlers.get(msg.getAction())).messageReceived(msg);
+    } else {
+      logger.finer(" (" + getObjectName() + ") there is no handler for message " + msg.getAction());
+    }
+  }
+
+  /** Наследующие классы должны перегрузить этот метод
+   * для регистрации обработчиков. */
+  protected void registerHandlers() {
+    /* DO NOTHING BY DEFAULT */
+  }
+
+  /** Метод вызываемый для очистки данных класса.
+   * @param type признак выхода
+   * @return код возврата
+   */
+  public int cleanUp(final int type) {
+    return 0;
+  }
+
+} // StandartODObject
