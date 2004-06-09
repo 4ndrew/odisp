534b0f24b08a51185d99423da7f6536c2f5e42e4
Valentin Alexeev <valentin.alekseev@gmail.com>
2004-06-09 18:23:12 +0000

    ODShutdownMessage restyled.

--- src/org/valabs/stdmsg/ODShutdownMessage.java
+++ src/org/valabs/stdmsg/ODShutdownMessage.java
@@ -1,63 +1,50 @@
 package com.novel.stdmsg;
 
+import com.novel.odisp.common.Message;
 /** Класс реализующий сообщение диспетчеру ODISP об останове.
  * Это сообщение должно обрабатыватся только диспетчером.
  * В качестве параметра можно указать код выхода который будет
  * рассылатся объектам.
  * @author <a href="mailto:valeks@novel-il.ru">Valentin A. Alekseev</a>
- * @author (C) 2003, НПП "Новел-ИЛ"
- * @version $Id: ODShutdownMessage.java,v 1.10 2004/04/02 09:54:49 valeks Exp $
+ * @author (C) 2003-2004, НПП "Новел-ИЛ"
+ * @version $Id: ODShutdownMessage.java,v 1.11 2004/06/09 18:23:12 valeks Exp $
  */
 
-public class ODShutdownMessage extends StandartMessage {
+public class ODShutdownMessage {
   /** Символьное имя сообщения. */
   public static final String NAME = "od_shutdown";
-
-  /** Код выхода. */
-  private transient int exitCode = 0;
   /** Индекс поля в сообщении. */
   private static final String EXITCODE_IDX = "0";
 
-  /** Создает новое сообщение с заданными параметрами.
+  /** Настраивает сообщение с заданными параметрами.
    * @param origin отправитель
    * @param replyTo индекс сообщения на которое производится ответ
    */
-  public ODShutdownMessage(final String origin, final int replyTo) {
-    super("od_shutdown", "dispatcher", origin, replyTo);
+  public static final void setup(final Message msg, final String origin, final int replyTo) {
+    msg.setAction(NAME);
+    msg.setDestination("dispatcher");
+    msg.setOrigin(origin);
+    msg.setReplyTo(replyTo);
+    msg.setRoutable(false);
+    msg.setCorrect(true);
   }
 
   /** Вернуть значение кода выхода.
    * @return код выхода
    */
-  public final int getExitCode() {
-    if (isCE()) {
-      return ((Integer) getField(EXITCODE_IDX)).intValue();
-    }
-    return exitCode;
+  public static final int getExitCode(final Message msg) {
+    return ((Integer) msg.getField(EXITCODE_IDX)).intValue();
   }
 
   /** Установить значение кода выхода.
    * @param newExitCode новое значение кода выхода
    * @return ссылка на сообщение
    */
-  public final ODShutdownMessage setExitCode(final int newExitCode) {
-    exitCode = newExitCode;
-    return this;
+  public static final void setExitCode(final Message msg, final int newExitCode) {
+    msg.addField(EXITCODE_IDX, new Integer(newExitCode));
   }
 
-  /** Проверка корректности сообщения.
-   * @return флаг корректности
-   */
-  public final boolean isCorrect() {
-    if (isCE()) {
-      return true;
-    }
-    getContents().clear();
-    addField(EXITCODE_IDX, new Integer(exitCode));
-    setCE(true);
-    return true;
-  }
-  public final boolean isRoutable() {
-    return false;
+  public static final boolean equals(final Message msg) {
+    return msg.getAction().equals(NAME);
   }
 } // ODShutdownMessage
