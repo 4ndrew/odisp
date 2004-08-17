cb5a62b064e20e9106855dbb7a26c54cd45d183e
Valentin Alexeev <valentin.alekseev@gmail.com>
2004-08-17 11:39:43 +0000

    Support for new info inquiry procedure.

--- src/org/valabs/stdobj/console/ConsoleObject.java
+++ src/org/valabs/stdobj/console/ConsoleObject.java
@@ -1,18 +1,32 @@
 package com.novel.stdobj.console;
 
+import java.util.ArrayList;
+
+import com.novel.nms.messages.ModuleAboutMessage;
+import com.novel.nms.messages.ModuleAboutReplyMessage;
+import com.novel.nms.messages.ModuleStatusMessage;
+import com.novel.nms.messages.ModuleStatusReplyMessage;
 import com.novel.odisp.common.Message;
 import com.novel.odisp.common.StandartODObject;
+import com.novel.stdmsg.ODCleanupMessage;
+import com.novel.stdmsg.ODObjectLoadedMessage;
 
 /** Объект ODISP реализующий консольный интерфейс доступа к менеджеру.
  * 
  * @author <a href="mailto:valeks@novel-il.ru">Валентин А. Алексеев</a>
  * @author <a href="mailto:dron@novel-il.ru">Андрей А. Порохин</a>
- * @author (C) 2003, НПП "Новел-ИЛ"
- * @version $Id: ConsoleObject.java,v 1.19 2004/06/25 13:15:31 dron Exp $
+ * @version $Id: ConsoleObject.java,v 1.20 2004/08/17 11:39:43 valeks Exp $
  */
 public class ConsoleObject extends StandartODObject {
   /** Имя объекта */
-  public final static String NAME = "console"; 
+  public final static String NAME = "console";
+  /** Полное название объекта. */
+  private static String FULLNAME = "ODISP Message Console"; 
+  /** Версия объекта. */
+  private static String VERSION = "0.3.0";
+  /** Дополнительная информация. */
+  private static String COPYRIGHT = "(C) 2003-2004 Valentin A. Alekseev, Andrew A. Porohin";
+  
   /** Поток читающий ввод с консоли. */
   private ConsoleReader reader;
 
@@ -22,11 +36,26 @@ public class ConsoleObject extends StandartODObject {
    */
   public final void handleMessage(final Message msg) {
     logger.finest("console object -- processing " + msg);
-    if (msg.getAction().equals("od_object_loaded")) {
+    if (ODObjectLoadedMessage.equals(msg)) {
       reader = new ConsoleReader(getObjectName(), dispatcher, logger);
       reader.start();
-    } else if (msg.getAction().equals("od_cleanup")) {
+    } else if (ODCleanupMessage.equals(msg)) {
       cleanUp(0);
+    } else if (ModuleAboutMessage.equals(msg)) {
+    	Message m = dispatcher.getNewMessage();
+    	ModuleAboutReplyMessage.setup(m, msg.getOrigin(), getObjectName(), msg.getId());
+    	ModuleAboutReplyMessage.setName(m, FULLNAME);
+    	ModuleAboutReplyMessage.setVersion(m, VERSION);
+    	ModuleAboutReplyMessage.setCopyright(m, COPYRIGHT);
+    	dispatcher.send(m);
+    } else if (ModuleStatusMessage.equals(msg)) {
+    	Message m = dispatcher.getNewMessage();
+    	ModuleStatusReplyMessage.setup(m, msg.getOrigin(), getObjectName(), msg.getId());
+    	ModuleStatusReplyMessage.setRunningState(m, "No error.");
+    	ModuleStatusReplyMessage.setRunningTasks(m, new ArrayList());
+    	ModuleStatusReplyMessage.setCompletedTasks(m, new ArrayList());
+    	ModuleStatusReplyMessage.setFailedTasks(m, new ArrayList());
+    	dispatcher.send(m);
     } else {
       System.out.println("Received:");
       System.out.println(msg.toString(true));
