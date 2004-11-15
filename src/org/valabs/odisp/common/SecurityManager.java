2721a83f88993356b2a795895560e4a5898d1bf5
Valentin Alexeev <valentin.alekseev@gmail.com>
2004-11-15 11:54:53 +0000

    Support for authentication in SecurityManager

--- src/org/valabs/odisp/common/SecurityManager.java
+++ src/org/valabs/odisp/common/SecurityManager.java
@@ -4,7 +4,7 @@ import java.util.Map;
 
 /** Интерфейс менеджера безопасности ODISP.
  * @author (C) 2004 <a href="valeks@novel-il.ru">Valentin A. Alekseev</a>
- * @version $Id: SecurityManager.java,v 1.3 2004/08/23 07:42:37 valeks Exp $
+ * @version $Id: SecurityManager.java,v 1.4 2004/11/15 11:54:53 valeks Exp $
  */
 public interface SecurityManager {
 	/** Проверка доступности пользователю заданного свойства.
@@ -13,4 +13,9 @@ public interface SecurityManager {
 	 * @param options Дополнительные опции
 	 */
 	boolean checkAccess(String userName, String capabilityName, Map options);	
+  /** Аутентифицировать пользователя.
+   * @param name имя пользователя
+   * @param password пароль пользователя
+   */
+  boolean authenticateUser(final String name, final String password);
 }
--- src/org/valabs/stdobj/nullauth/NullAuth.java
+++ src/org/valabs/stdobj/nullauth/NullAuth.java
@@ -12,25 +12,21 @@ import org.valabs.stdmsg.ODObjectLoadedMessage;
  * файле classes.cfg. Возможно лишь два варианта поведения -- default to deny или
  * default to accept.
  * @author (C) 2004 <a href="mailto:valeks@novel-il.ru">Валентин А. Алексеев</a>
- * @version $Id: NullAuth.java,v 1.3 2004/08/23 07:42:38 valeks Exp $
+ * @version $Id: NullAuth.java,v 1.4 2004/11/15 11:54:53 valeks Exp $
  */
 public class NullAuth extends StandartODObject implements SecurityManager {
-	public static final String NAME = "nullauth";
-	
-	public NullAuth(final Integer id) {
-		super(NAME + id.toString());
-	}
+
+  public static final String NAME = "nullauth";
+
+  public NullAuth(final Integer id) {
+    super(NAME + id.toString());
+  }
+
   /* (non-Javadoc)
    * @see org.valabs.odisp.common.SecurityManager#checkAccess(java.lang.String, java.lang.String, java.util.Map)
    */
-  public boolean checkAccess(
-    String userName,
-    String capabilityName,
-    Map options) {
-    if (getParameter("default", "deny") != null 
-    	&& getParameter("default", "deny").equals("deny")) {
-    		return false;
-    }
+  public boolean checkAccess(String userName, String capabilityName, Map options) {
+    if (getParameter("default", "deny") != null && getParameter("default", "deny").equals("deny")) { return false; }
     return true;
   }
 
@@ -38,9 +34,7 @@ public class NullAuth extends StandartODObject implements SecurityManager {
    * @see org.valabs.odisp.common.ODObject#getDepends()
    */
   public String[] getDepends() {
-    String[] depends = {
-    	"dispatcher"
-    };
+    String[] depends = { "dispatcher" };
     return depends;
   }
 
@@ -48,12 +42,21 @@ public class NullAuth extends StandartODObject implements SecurityManager {
    * @see org.valabs.odisp.common.ODObject#getProviding()
    */
   public String[] getProviding() {
-    String[] providing = { NAME, "authmanager"};
+    String[] providing = { NAME, "authmanager" };
     return providing;
   }
+
   public void handleMessage(Message msg) {
-  	if (ODObjectLoadedMessage.equals(msg)) {
-  		dispatcher.addSecurityManager(this);
-  	}
+    if (ODObjectLoadedMessage.equals(msg)) {
+      dispatcher.addSecurityManager(this);
+    }
+  }
+
+  /* (non-Javadoc)
+   * @see org.valabs.odisp.common.SecurityManager#authenticateUser(java.lang.String, java.lang.String)
+   */
+  public boolean authenticateUser(String name, String password) {
+    if (getParameter("default", "deny") != null && getParameter("default", "deny").equals("deny")) { return false; }
+    return true;
   }
 }
