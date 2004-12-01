e09d11d003bd0c067b04141cb7f56145b0c955a8
Boris Volkovyski <boris.volkovyski@mac.com>
2004-12-01 10:36:42 +0000

    CHANGED codestyle

--- src/org/valabs/odisp/standart/ConfigurationManager.java
+++ src/org/valabs/odisp/standart/ConfigurationManager.java
@@ -15,71 +15,88 @@ import org.valeks.xlang.parser.Parser;
 import org.valeks.xlang.parser.Tag;
 import org.valeks.xlang.parser.XLangException;
 
-
-/** Реализация менеджера конфигурации.
- * @author (C) 2004 <a href="valeks@valabs.spb.ru">Валентин А. Алексеев</a>
- * @version $Id: ConfigurationManager.java,v 1.3 2004/11/30 07:42:09 boris Exp $
+/**
+ * Реализация менеджера конфигурации.
+ * 
+ * @author (C) 2004 <a href="valeks@valabs.spb.ru">Валентин А. Алексеев </a>
+ * @version $Id: ConfigurationManager.java,v 1.4 2004/12/01 10:36:42 boris Exp $
  */
 class ConfigurationManager implements org.valabs.odisp.common.ConfigurationManager {
+
   List objects = new ArrayList();
+
   List resources = new ArrayList();
+
   MultiMap params = new MultiMap();
+
   Logger log = Logger.getLogger(ConfigurationManager.class.getName());
-  
+
   public ConfigurationManager() {
     log.setLevel(Level.ALL);
   }
-  
-  /* (non-Javadoc)
+
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.valabs.odisp.common.ConfigurationManager#supportComponentListing()
    */
   public boolean supportComponentListing() {
     return true;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.valabs.odisp.common.ConfigurationManager#getResourceList()
    */
   public List getResourceList() {
     return resources;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.valabs.odisp.common.ConfigurationManager#getObjectList()
    */
   public List getObjectList() {
     return objects;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.valabs.odisp.common.ConfigurationManager#supportParameterFetching()
    */
   public boolean supportParameterFetching() {
     return true;
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.valabs.odisp.common.ConfigurationManager#getParameter(java.lang.String, java.lang.String)
    */
   public String getParameter(String domain, String paramName) {
     return params.get(domain, paramName);
   }
 
-  /* (non-Javadoc)
+  /*
+   * (non-Javadoc)
+   * 
    * @see org.valabs.odisp.common.ConfigurationManager#setCommandLineArguments(java.lang.String[])
    */
   public void setCommandLineArguments(String[] args) {
     try {
-	    InputStream inp = new FileInputStream(args[0]);
-        Parser p = new Parser(inp);
-	    loadConfiguration(p.getRootTag());
-      } catch (FileNotFoundException e) {
-	    log.severe("configuration file " + args[0] + " not found.");
-      } catch (XLangException e) {
-        log.severe("configuration file " + args[0] + " contains unrecoverable errors: " + e);
-      }
+      InputStream inp = new FileInputStream(args[0]);
+      Parser p = new Parser(inp);
+      loadConfiguration(p.getRootTag());
+    } catch (FileNotFoundException e) {
+      log.severe("configuration file " + args[0] + " not found.");
+    } catch (XLangException e) {
+      log.severe("configuration file " + args[0] + " contains unrecoverable errors: " + e);
+    }
   }
-  
+
   private void loadConfiguration(Tag docTag) {
     Iterator it = docTag.getChild().iterator();
     params.putAll("root", getParamsForTag(docTag));
@@ -92,7 +109,8 @@ class ConfigurationManager implements org.valabs.odisp.common.ConfigurationManag
           continue;
         }
         Map lparams = getParamsForTag(curt);
-        objects.add(new org.valabs.odisp.common.ConfigurationManager.ComponentConfiguration(className, lparams));
+        objects.add(new org.valabs.odisp.common.ConfigurationManager.ComponentConfiguration(className,
+                lparams));
         params.putAllPrefixed("boot", className, lparams);
       } else if (curt.getName().equalsIgnoreCase("resource")) {
         String className = (String) curt.getAttributes().get("name");
@@ -101,15 +119,17 @@ class ConfigurationManager implements org.valabs.odisp.common.ConfigurationManag
           continue;
         }
         Map lparams = getParamsForTag(curt);
-        resources.add(new org.valabs.odisp.common.ConfigurationManager.ComponentConfiguration(className, lparams));
+        resources.add(new org.valabs.odisp.common.ConfigurationManager.ComponentConfiguration(className,
+                lparams));
         params.putAllPrefixed("boot", className, lparams);
       }
     }
     log.fine("Found " + resources.size() + " resources and " + objects.size() + " objects to load.");
   }
 
-
-  /** Получить информацию о параметрах для заданного тега.
+  /**
+   * Получить информацию о параметрах для заданного тега.
+   * 
    * @param childTag тег
    */
   private Map getParamsForTag(final Tag childTag) {
@@ -133,22 +153,24 @@ class ConfigurationManager implements org.valabs.odisp.common.ConfigurationManag
   }
 
   class MultiMap {
+
     private Map domains = new HashMap();
+
     public void put(final String domainName, final String param, final String value) {
       getDomain(domainName).put(param, value);
     }
-    
+
     public String get(final String domainName, final String paramName) {
       return (String) getDomain(domainName).get(paramName);
     }
-    
+
     public void putAll(final String domainName, final Map params) {
       getDomain(domainName).putAll(params);
     }
-    
+
     public void putAllPrefixed(final String domainName, final String prefix, final Map params) {
       //Не стоит забывать что параметров может и не быть ;)
-      if(params == null)return;
+      if (params == null) { return; }
 
       Map domain = getDomain(domainName);
       Iterator keyIt = params.keySet().iterator();
@@ -157,7 +179,7 @@ class ConfigurationManager implements org.valabs.odisp.common.ConfigurationManag
         domain.put(prefix + key, params.get(key));
       }
     }
-    
+
     public Map getDomain(final String domain) {
       Map result;
       if (domains.containsKey(domain)) {
