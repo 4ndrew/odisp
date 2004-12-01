a2ea22c062d839838353be559712bc0a8a0e6a03
Boris Volkovyski <boris.volkovyski@mac.com>
2004-12-01 10:35:32 +0000

    CHANGED handleMessage() code is more readable now

--- src/org/valabs/odisp/common/StandartODObject.java
+++ src/org/valabs/odisp/common/StandartODObject.java
@@ -9,44 +9,62 @@ import java.util.logging.Logger;
 
 import org.valabs.odisp.SessionManager;
 
-/** Стандартный объект ODISP.
- * @author (C) 2004 <a href="mailto:valeks@novel-il.ru">Valentin A. Alekseev</a>
- * @version $Id: StandartODObject.java,v 1.12 2004/11/05 12:11:41 valeks Exp $
+/**
+ * Стандартный объект ODISP.
+ * 
+ * @author (C) 2004 <a href="mailto:valeks@novel-il.ru">Valentin A. Alekseev </a>
+ * @version $Id: StandartODObject.java,v 1.13 2004/12/01 10:35:32 boris Exp $
  */
 
 public abstract class StandartODObject implements ODObject {
+
   /** Журнал. */
   protected Logger logger;
+
   /** Диспетчер работающий с этим объектом. */
   protected Dispatcher dispatcher;
+
   /** Список сообщений к обработке. */
   private List messages;
+
   /** Таблица конфигурационных параметров. */
   private Map configuration;
-  /** Regex маска принимаемых сообщений.
-   * По умолчанию инициализируется именем объекта. */
+
+  /**
+   * Regex маска принимаемых сообщений. По умолчанию инициализируется именем объекта.
+   */
   private String match;
+
   /** Внутреннее имя объекта в ядре ODISP. */
   private String name;
+
   /** Карта обработчиков сообщений. */
   private Map handlers;
+
   /** Признак блокировки объекта. */
   private boolean blockedState = false;
-  /** Изменить маску принимаемых сообщений.
+
+  /**
+   * Изменить маску принимаемых сообщений.
+   * 
    * @param newMatch новая маска
    */
   protected final void setMatch(final String newMatch) {
     match = newMatch;
   }
 
-  /** Доступ к RegEx выражению совпадения адреса получателя.
+  /**
+   * Доступ к RegEx выражению совпадения адреса получателя.
+   * 
    * @return строка с regex
    */
   public final String getMatch() {
     return match;
   }
 
-  /** Конструктор инициализирующий почтовый ящик.
+  /**
+   * Конструктор инициализирующий почтовый ящик.
+   * 
    * @param newName имя объекта
    */
   public StandartODObject(final String newName) {
@@ -59,43 +77,55 @@ public abstract class StandartODObject implements ODObject {
     registerHandlers();
   }
 
-  /** Доступ к диспетчеру.
+  /**
+   * Доступ к диспетчеру.
+   * 
    * @return ссылка на диспетчер
    */
   protected final Dispatcher getDispatcher() {
     return dispatcher;
   }
 
-  /** Возвращает внутреннее ODISP имя объекта.
+  /**
+   * Возвращает внутреннее ODISP имя объекта.
+   * 
    * @return ODISP имя объекта
    */
   public final String getObjectName() {
     return name;
   }
 
-  /** Устанавливает диспетчера для текущего объекта.
+  /**
+   * Устанавливает диспетчера для текущего объекта.
+   * 
    * @param d диспетчер работающий с этим объектом
    */
   public final void setDispatcher(final Dispatcher d) {
     this.dispatcher = d;
   }
 
-  /** Установить таблицу параметров.
+  /**
+   * Установить таблицу параметров.
+   * 
    * @param cfg новая таблица
    */
   public void setConfiguration(final Map cfg) {
     configuration = cfg;
   }
-  /** Получить значение параметра конфигурации.
+
+  /**
+   * Получить значение параметра конфигурации.
+   * 
    * @param name имя параметра
    */
   protected final String getParameter(final String name) {
-    if (configuration != null && configuration.containsKey(name)) {
-      return (String) configuration.get(name);
-    }
+    if (configuration != null && configuration.containsKey(name)) { return (String) configuration.get(name); }
     return null;
   }
-  /** Получить значение параметра конфигурации с учетом значения по-умолчанию.
+
+  /**
+   * Получить значение параметра конфигурации с учетом значения по-умолчанию.
+   * 
    * @param name имя параметра
    * @param defValue значение по умолчанию
    */
@@ -103,19 +133,20 @@ public abstract class StandartODObject implements ODObject {
     return getParameter(name) == null ? defValue : getParameter(name);
   }
 
-  /** Регистрация обработчика сообщения.
+  /**
+   * Регистрация обработчика сообщения.
+   * 
    * @param message символьное имя сообщения
    * @param handler обработчик
    */
-  protected final void addHandler(final String message,
-				  final MessageHandler handler) {
-    if (handlers.containsKey(message)) {
-      return;
-    }
+  protected final void addHandler(final String message, final MessageHandler handler) {
+    if (handlers.containsKey(message)) { return; }
     handlers.put(message, handler);
   }
 
-  /** Обработка сообщения.
+  /**
+   * Обработка сообщения.
+   * 
    * @param msg сообщение для обработки
    */
   public void handleMessage(final Message msg) {
@@ -124,51 +155,65 @@ public abstract class StandartODObject implements ODObject {
       messages.add(msg);
       return;
     }
-    if (!SessionManager.getSessionManager().processMessage(msg) && handlers.containsKey(msg.getAction())) {
-      ((MessageHandler) handlers.get(msg.getAction())).messageReceived(msg);
-    } else {
+    boolean handled = false;
+
+    if (SessionManager.getSessionManager().processMessage(msg)) {
+      handled = true;
+    }
+    if (handlers.containsKey(msg.getAction())) {
+      if (!handled) {
+        ((MessageHandler) handlers.get(msg.getAction())).messageReceived(msg);
+        handled = true;
+      }
+    }
+    if (!handled) {
       logger.finer(" (" + getObjectName() + ") there is no handler for message " + msg.getAction());
     }
   }
 
-  /** Наследующие классы должны перегрузить этот метод
-   * для регистрации обработчиков. */
+  /**
+   * Наследующие классы должны перегрузить этот метод для регистрации обработчиков.
+   */
   protected void registerHandlers() {
     /* DO NOTHING BY DEFAULT */
   }
 
-  /** Метод вызываемый для очистки данных класса.
+  /**
+   * Метод вызываемый для очистки данных класса.
+   * 
    * @param type признак выхода
    * @return код возврата
    */
   public int cleanUp(final int type) {
     return 0;
   }
-  
-  /** Установить состояние блокировки.
-   * В этом состоянии все сообщения, которые были получены объектом (кроме
-   * сообщений с префиксом "od_"), сохраняются для последующей обработки.
-   * При смене состояния на "неблокирующее" сохраненные сообщения передаются
-   * на обработку отдельному Sender-потоку.
+
+  /**
+   * Установить состояние блокировки. В этом состоянии все сообщения, которые были получены объектом (кроме
+   * сообщений с префиксом "od_"), сохраняются для последующей обработки. При смене состояния на
+   * "неблокирующее" сохраненные сообщения передаются на обработку отдельному Sender-потоку.
+   * 
    * @param newState новое состояние
    */
   protected void setBlockedState(final boolean newState) {
-  	blockedState = newState;
-  	if (!blockedState) {
-  		new Thread("sender-message-flush") {
-  		List localMessages;
-  		public void run() {
-  			synchronized (messages) {
-  				localMessages = new ArrayList(messages);
-  				messages.clear();
-  			}
-  			Iterator it = localMessages.iterator();
-  			while (it.hasNext()) {
-  				Message elt = (Message) it.next();
-  				handleMessage(elt);
-  			}
-  		}
-  		}.start();
-  	}
+    blockedState = newState;
+    if (!blockedState) {
+      new Thread("sender-message-flush") {
+
+        List localMessages;
+
+        public void run() {
+          synchronized (messages) {
+            localMessages = new ArrayList(messages);
+            messages.clear();
+          }
+          Iterator it = localMessages.iterator();
+          while (it.hasNext()) {
+            Message elt = (Message) it.next();
+            handleMessage(elt);
+          }
+        }
+      }.start();
+    }
   }
 } // StandartODObject
