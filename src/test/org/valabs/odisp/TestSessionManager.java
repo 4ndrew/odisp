80dc067f3739efbe60d7fe728b13d54e25e66602
Valentin Alexeev <valentin.alekseev@gmail.com>
2004-12-02 21:47:15 +0000

    First sights of unit testing. This test consist of 3 cases: simple single message processing,multiply messages with the same replyId, modification of session manager state from within message handler registred fired from session manager.To run tests: add JUNIT into project's classpath in Eclipse, highlight file in package explorer and select Run > Run As > Run as JUnit Test.

---
+++ src/test/org/valabs/odisp/TestSessionManager.java
@@ -0,0 +1,74 @@
+package test.org.valabs.odisp;
+
+import junit.framework.TestCase;
+
+import org.doomdark.uuid.UUID;
+import org.valabs.odisp.SessionManager;
+import org.valabs.odisp.common.Message;
+import org.valabs.odisp.common.MessageHandler;
+import org.valabs.stdmsg.StandartMessage;
+
+
+/** Набор тестов для менеджера сессий.
+ * @author <a href="mailto:valeks@novel-il.ru">Алексеев Валентин А.</a>
+ * @author (C) 2004 НПП "Новел-ИЛ"
+ * @version $Id: TestSessionManager.java,v 1.1 2004/12/02 21:47:15 valeks Exp $
+ */
+public class TestSessionManager extends TestCase {
+  /** Тестирование простейшего случая -- обработчик на одиночное сообщение. */
+  public void testSimpleHandler() {
+    SessionManager sm = SessionManager.getSessionManager();
+    MessageHandler mh = new MessageHandler() {
+      public void messageReceived(final Message msg) {
+        
+      }
+    };
+    sm.addMessageListener(UUID.getNullUUID(), mh);
+    Message m = new StandartMessage();
+    m.setReplyTo(UUID.getNullUUID());
+    assertEquals(true, sm.processMessage(m));
+    assertEquals(false, sm.processMessage(m));
+  }
+  
+  /** Тестирование возможности задания обработчика с ручным удалением. */
+  public void testMultiplyCalls() {
+    SessionManager sm = SessionManager.getSessionManager();
+    MessageHandler mh = new MessageHandler() {
+      public void messageReceived(final Message msg) {
+        
+      }
+    };
+    sm.addMessageListener(UUID.getNullUUID(), mh, true);
+    Message m = new StandartMessage();
+    m.setReplyTo(UUID.getNullUUID());
+    assertEquals(true, sm.processMessage(m) && sm.processMessage(m));
+    sm.removeMessageListener(UUID.getNullUUID(), mh);
+    assertEquals(false, sm.processMessage(m));
+  }
+  
+  /** Тестирование на возможность модификации данных о сессиях изнутри обработчика, вызванного
+   * SessionManager'ом (в память о существовавшей ошибке). */
+  public void testModifySMFromHandler() {
+    SessionManager sm = SessionManager.getSessionManager();
+    MessageHandler mh = new MessageHandler() {
+      public void messageReceived(final Message msg) {
+        SessionManager.getSessionManager().addMessageListener(UUID.getNullUUID(), new MessageHandler() {
+          public void messageReceived(final Message msg) {
+            
+          }
+        });
+      }
+    };
+    sm.addMessageListener(UUID.getNullUUID(), mh);
+    Message m = new StandartMessage();
+    m.setReplyTo(UUID.getNullUUID());
+    assertEquals(true, sm.processMessage(m));
+    assertEquals(true, sm.processMessage(m));
+    assertEquals(false, sm.processMessage(m));
+  }
+  
+  public static void main(String[] args) {
+    junit.textui.TestRunner.run(TestSessionManager.class);
+  }
+
+}
