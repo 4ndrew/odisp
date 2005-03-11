0fe3d1b136043027bf8c868c5d1c9cb222341117
Valentin Alexeev <valentin.alekseev@gmail.com>
2005-03-11 12:57:19 +0000

    Fixup loop variable bug. Remove dataThread.

--- src/org/valabs/odisp/standart/ResourceManager.java
+++ src/org/valabs/odisp/standart/ResourceManager.java
@@ -13,13 +13,13 @@ import org.valabs.odisp.common.Resource;
 
 /** Менеджер ресурсных объектов ODISP.
  * @author (C) 2004 <a href="mailto:valeks@novel-il.ru">Valentin A. Alekseev</a>
- * @version $Id: ResourceManager.java,v 1.36 2005/02/27 12:37:31 valeks Exp $
+ * @version $Id: ResourceManager.java,v 1.37 2005/03/11 12:57:19 valeks Exp $
  */
 class ResourceManager implements org.valabs.odisp.common.ResourceManager {
   /** Ссылка на диспетчер объектов. */
   private final Dispatcher dispatcher;
   /** Нить обработки запросов. */
-  private final DataThread dataThread;
+  private final Map resources = new HashMap();
 
   /** Журнал. */
   private static final Logger log = Logger.getLogger(ResourceManager.class.getName());
@@ -28,7 +28,7 @@ class ResourceManager implements org.valabs.odisp.common.ResourceManager {
    * @return список ресурсов
    */
   public final Map getResources() {
-    return dataThread.getResources();
+    return resources;
   }
 
   /** Динамическая загрузка ресурсных объектов.
@@ -45,14 +45,14 @@ class ResourceManager implements org.valabs.odisp.common.ResourceManager {
       realMult = 1;
     }
     logMessage += className;
-    for (int i = 0; i < mult; i++) {
+    for (int i = 0; i < realMult; i++) {
       try {
         final Resource r = (Resource) Class.forName(className).newInstance();
         r.setConfiguration(config);
         re.addResource(r);
         logMessage += "+";
-        synchronized (dataThread) {
-          dataThread.getResources().put(className, re);
+        synchronized (resources) {
+          resources.put(className, re);
         }
         logMessage += " ok";
         log.config(logMessage);
@@ -72,103 +72,17 @@ class ResourceManager implements org.valabs.odisp.common.ResourceManager {
   public ResourceManager(final Dispatcher newDispatcher) {
     log.setLevel(java.util.logging.Level.ALL);
     dispatcher = newDispatcher;
-    dataThread = new DataThread(newDispatcher);
   }
 
   public List statRequest() {
     final List result = new ArrayList();
-    Iterator it = dataThread.getResources().keySet().iterator();
+    Iterator it = resources.keySet().iterator();
     while (it.hasNext()) {
-      result.add(dataThread.getResources().get(it.next()).toString());
-    }
-    it = dataThread.getRequestList().iterator();
-    result.add("DataThread request queue...");
-    while (it.hasNext()) {
-      result.add(((RequestListEntry) it.next()).toString());
+      result.add(resources.get(it.next()).toString());
     }
     return result;
   }
 
-
-  /** Интерфейс описывающий запрос выполняемый DataThread. */
-  private interface RequestListEntry {
-    /** Выполнение запроса.
-     * @param data нить данных на которой выполняется запрос
-     * @return флаг возможности удаления запроса
-     */
-    public boolean performAction(final DataThread data);
-    /** Строковое описание класса. */
-    public String toString();
-  } // RequestListEntry
-
-  /** Отдельная нить обработки запросов на захват-высвобождение в стиле DataThread,
-   * которая гарантирует синхронность обращение к списку ресурсов.
-   */
-  private class DataThread extends Thread {
-    /** Список ресурсов. */
-    private Map resources = new HashMap();
-    /** Доступ к списку ресурсов. */
-    public Map getResources() {
-      return resources;
-    }
-    /** Хранилище запросов. */
-    private List requestList = new ArrayList();
-    /** Доступ на чтение к списку запросов. */
-    public List getRequestList () {
-      return Collections.unmodifiableList(requestList);
-    }
-    /** Ссылка на диспетчера. */
-    private Dispatcher dispatcher = null;
-    /** Доступ для запросов к диспетчеру. */
-    public Dispatcher getDispatcher() {
-      return dispatcher;
-    }
-    /** Добавление запроса в очередь.
-     * @param req запрос
-     */
-    public void addRequest(RequestListEntry req) {
-      synchronized (requestList) {
-	requestList.add(req);
-      }
-      synchronized (this) {
-        notify();
-      }
-    }
-    /** Конструктор инициализирующий и запускающий нить.
-     * @param nDispatcher ссылка на диспетчер
-     */
-    public DataThread(final Dispatcher nDispatcher) {
-      super("Resource requests handler");
-      dispatcher = nDispatcher;
-      setDaemon(true);
-      this.start();
-    }
-
-    /** Метод, который циклически просматривает и выполняет запросы из очереди. */
-    public void run() {
-      while (true) {
-	try {
-	  synchronized (this) {
-	    wait(100);
-	  }
-	} catch (InterruptedException e) {}
-	synchronized (requestList) {
-	  /** TODO: переделать на несколько наборов так, что бы не пытатся исполнять заведомо невыполнимые запросы. */
-          List localList = new ArrayList(requestList);
-	  Iterator it = localList.iterator();
-	  List toRemove = new ArrayList();
-	  while (it.hasNext()) {
-	    RequestListEntry rle = (RequestListEntry) it.next();
-	    if(rle.performAction(this)) {
-	      // сохранять в очередь на удаление только отработавшие запросы
-	      toRemove.add(rle);
-	    }
-	  }
-	  requestList.removeAll(toRemove);
-	}
-      }
-    }
-  } // DataThread
 
   /**
    * @see org.valabs.odisp.common.ResourceManager#resourceAcquire(java.lang.String)
@@ -191,8 +105,7 @@ class ResourceManager implements org.valabs.odisp.common.ResourceManager {
    */
   public Resource resourceTryAcquire(final String className) {
     Resource result = null;
-    synchronized (dataThread) {
-      final Map resources = dataThread.getResources();
+    synchronized (resources) {
       if (resources.containsKey(className)) {
         final ResourceEntry re = (ResourceEntry) resources.get(className);
         if (re.isAvailable()) {
@@ -207,8 +120,7 @@ class ResourceManager implements org.valabs.odisp.common.ResourceManager {
    * @see org.valabs.odisp.common.ResourceManager#releaseResource(java.lang.String, org.valabs.odisp.common.Resource)
    */
   public void releaseResource(final String className, final Resource resource) {
-    synchronized (dataThread) {
-      final Map resources = dataThread.getResources();
+    synchronized (resources) {
       if (resources.containsKey(className)) {
         ((ResourceEntry) resources.get(className)).releaseResource(resource);
       }
