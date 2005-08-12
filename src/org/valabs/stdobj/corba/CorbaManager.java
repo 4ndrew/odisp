2199fc7ad7ca2c5b4cce4c5e65223fe692a91732
Valentin Alexeev <valentin.alekseev@gmail.com>
2005-08-12 09:55:21 +0000

    Bugfix: move ORB to another thread.

--- src/org/valabs/stdobj/corba/CorbaManager.java
+++ src/org/valabs/stdobj/corba/CorbaManager.java
@@ -1,8 +1,5 @@
 package org.valabs.stdobj.corba;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.omg.CORBA.ORB;
 import org.omg.CORBA.ORBPackage.InvalidName;
 import org.omg.CosNaming.NameComponent;
@@ -14,8 +11,6 @@ import org.omg.PortableServer.POA;
 import org.omg.PortableServer.POAHelper;
 import org.omg.PortableServer.Servant;
 import org.omg.PortableServer.POAManagerPackage.AdapterInactive;
-import org.omg.PortableServer.POAPackage.ServantNotActive;
-import org.omg.PortableServer.POAPackage.WrongPolicy;
 import org.valabs.odisp.common.Message;
 import org.valabs.odisp.common.MessageHandler;
 import org.valabs.odisp.common.StandartODObject;
@@ -26,7 +21,7 @@ import org.valabs.stdmsg.corba.CorbaAddServiceMessage;
  * инициализацию сервисов и запуск всех необходимых 
  * серверных процессов.
  * @author <a href="mailto:valeks@valabs.spb.ru">Алексеев Валентин А.</a>
- * @version $Id: CorbaManager.java,v 1.1 2005/08/11 21:45:11 valeks Exp $
+ * @version $Id: CorbaManager.java,v 1.2 2005/08/12 09:55:21 valeks Exp $
  */
 public class CorbaManager extends StandartODObject implements MessageHandler {
 
@@ -35,10 +30,7 @@ public class CorbaManager extends StandartODObject implements MessageHandler {
     public static final String VERSION = "0.1.0";
     public static final String COPYRIGHT = "(C) 2005 Valentin A. Alekseev";
     
-    private Map services = new HashMap();
-    private ORB anOrb;
-    private NamingContextExt ncRef;
-    private POA rootpoa;
+    private CorbaORB orb;
     
     protected void registerHandlers() {
         addHandler(CorbaAddServiceMessage.NAME, this);
@@ -47,6 +39,28 @@ public class CorbaManager extends StandartODObject implements MessageHandler {
 
     public void messageReceived(Message msg) {
         if (ODObjectLoadedMessage.equals(msg)) {
+        	orb = new CorbaORB();
+        } else if (CorbaAddServiceMessage.equals(msg)) {
+            String serviceName = CorbaAddServiceMessage.getServiceName(msg);
+            Servant serviceImpl = CorbaAddServiceMessage.getServiceImpl(msg);
+            orb.addService(serviceName, serviceImpl);
+        }
+    }
+    
+    public int cleanUp(int type) {
+    	if (orb != null) {
+    		orb.shutdown();
+    	}
+    	return type;
+    }
+    
+    class CorbaORB extends Thread {
+        private ORB anOrb;
+        private NamingContextExt ncRef;
+        private POA rootpoa;
+
+    	public CorbaORB() {
+    		super("Corba Manager CorbaORB");
             String orbArgs[] = { "-ORBInitialPort", "50000", };
             // create and initialize the ORB
             anOrb = ORB.init(orbArgs, null);
@@ -67,18 +81,23 @@ public class CorbaManager extends StandartODObject implements MessageHandler {
             } catch (AdapterInactive e) {
                 dispatcher.getExceptionHandler().signalException(e);
             }
-        } else if (CorbaAddServiceMessage.equals(msg)) {
-            String serviceName = CorbaAddServiceMessage.getServiceName(msg);
-            Servant serviceImpl = CorbaAddServiceMessage.getServiceImpl(msg);
+            this.start();
+    	}
+    	
+    	public void run() {
+    		anOrb.run();
+    	}
+    	
+    	public synchronized void shutdown() {
+    		logger.fine("Shutting down CorbaORB");
+    		anOrb.shutdown(true);
+    	}
+    	
+    	public synchronized void addService(String serviceName, Servant serviceImpl) {
             try {
-                org.omg.CORBA.Object ref = rootpoa.servant_to_reference(serviceImpl);
                 NameComponent path[] = ncRef.to_name(serviceName);
                 ncRef.rebind(path, serviceImpl._this_object(anOrb));
                 logger.config("Added CORBA service " + serviceName);
-            } catch (ServantNotActive e) {
-                dispatcher.getExceptionHandler().signalException(e);
-            } catch (WrongPolicy e) {
-                dispatcher.getExceptionHandler().signalException(e);
             } catch (org.omg.CosNaming.NamingContextPackage.InvalidName e) {
                 dispatcher.getExceptionHandler().signalException(e);
             } catch (NotFound e) {
@@ -86,7 +105,8 @@ public class CorbaManager extends StandartODObject implements MessageHandler {
             } catch (CannotProceed e) {
                 dispatcher.getExceptionHandler().signalException(e);
             }
-        }
+
+    	}
     }
     
     public CorbaManager() {
